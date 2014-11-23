198c1e7774d4f5ee4eed56796f3155d43cecf5c6
Michael Anderson <andermic@gmail.com>
2014-11-22 19:40:35 -0700

    Add backbone app for consuming rest api.

--- tinsparrow/static/js/tinsparrow.js
+++ tinsparrow/static/js/tinsparrow.js
@@ -1,57 +1,72 @@
-var SONG_SELECTOR = '.js-song',
-    PLAYER_SELECTOR = '#audio-player';
+var app = (function ($) {
+    var app = {
+        "models": {},
+        "collections": {},
+        "views": {},
+        "router": null
+    };
 
-QueueView = Backbone.View.extend({
-    el: '.js-queue-list',
-    events: {
-        "click .js-song": "playSong"
-    },
-    initialize: function () {
-        this.$player = $(PLAYER_SELECTOR);
-        this.$songs = this.$el.find(SONG_SELECTOR);
-        this.audioPlayer = this.$player.get(0);
-    },
-    playSong: function () {
-        var $song = $(event.target);
-
-        this.$songs.removeClass('active');
-        $song.addClass('active');
-
-        this.$player.empty().append(
-            '<source src="' + $song.data('url') + '" type="audio/mp4">'
-        );
-
-        this.audioPlayer.load();
-        this.audioPlayer.play();
-    }
-});
-
-qv = new QueueView();
-
-ArtistModel = Backbone.Model.extend({
-    urlRoot: '/api/artists',
-    url: function() {
-        return this.urlRoot + '/' + this.id + '/';
-    }
-});
-
-LibraryArtistView = Backbone.View.extend({
-    initialize: function() {
-        this.listenTo(this.model, 'change', this.render)
-    },
-
-    render: function(){
-        console.log(this.model);
-        this.$el.html(this.model.attributes.name);
-    }
-});
-
-var model = new ArtistModel({ id: 1 });
-model.fetch();
-
-$(document).ready(function() {
-    var artist = new LibraryArtistView({
-        el: $('.artist').first(),
-        model: model
+    $(document).ready(function() {
+        var router = new app.router();
     });
-});
+
+    return app;
+})(jQuery);
+
+//var SONG_SELECTOR = '.js-song',
+//    PLAYER_SELECTOR = '#audio-player';
+//
+//QueueView = Backbone.View.extend({
+//    el: '.js-queue-list',
+//    events: {
+//        "click .js-song": "playSong"
+//    },
+//    initialize: function () {
+//        this.$player = $(PLAYER_SELECTOR);
+//        this.$songs = this.$el.find(SONG_SELECTOR);
+//        this.audioPlayer = this.$player.get(0);
+//    },
+//    playSong: function () {
+//        var $song = $(event.target);
+//
+//        this.$songs.removeClass('active');
+//        $song.addClass('active');
+//
+//        this.$player.empty().append(
+//            '<source src="' + $song.data('url') + '" type="audio/mp4">'
+//        );
+//
+//        this.audioPlayer.load();
+//        this.audioPlayer.play();
+//    }
+//});
+//
+//qv = new QueueView();
+//
+//ArtistModel = Backbone.Model.extend({
+//    urlRoot: '/api/artists',
+//    url: function() {
+//        return this.urlRoot + '/' + this.id + '/';
+//    }
+//});
+//
+//LibraryArtistView = Backbone.View.extend({
+//    initialize: function() {
+//        this.listenTo(this.model, 'change', this.render)
+//    },
+//
+//    render: function(){
+//        console.log(this.model);
+//        this.$el.html(this.model.attributes.name);
+//    }
+//});
+//
+//var model = new ArtistModel({ id: 1 });
+//model.fetch();
+//
+//$(document).ready(function() {
+//    var artist = new LibraryArtistView({
+//        el: $('.artist').first(),
+//        model: model
+//    });
+//});
